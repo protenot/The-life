c088280e8fa08a87783b448f1f03d8cec369c508
Olga_Belaya <protenot@yandex.ru>
2023-07-01 13:45:22 +0300

    prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,34 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+    "jest/globals": true,
+  },
+  extends: ["standard-with-typescript", "prettier"],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      parserOptions: {
+        project: ["./tsconfig.json"],
+      },
     },
-    "extends": "standard-with-typescript",
-    "overrides": [
-        {
-        "files": ['*.ts', '*.tsx'],
-        "extends": [
-            'plugin:@typescript-eslint/recommended',
-            'plugin:@typescript-eslint/recommended-requiring-type-checking',
-          ], 
-          "parserOptions": {
-            "project": ['./tsconfig.json'],
-          },
-        }, 
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-       "project": ["tsconfig.json"]
-    },
-    "parser":'@typescript-eslint/parser',
-    "plugins": ["@typescript-eslint"],
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    project: ["tsconfig.json"],
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "jest"],
 
-    "rules": {
-        "import/no-unresolved": "off",
-        "import/extensions": ["warn", "never"],
-        "@typescript-eslint/prefer-nullish-coalescing": "off",
-        "@typescript-eslint/strict-boolean-expressions":"off"
-    }
-}
+  rules: {
+    "import/prefer-default-export": "off",
+    "import/no-unresolved": "off",
+    "import/extensions": ["warn", "never"],
+    "@typescript-eslint/prefer-nullish-coalescing": "off",
+    "@typescript-eslint/strict-boolean-expressions": "off",
+  },
+};
